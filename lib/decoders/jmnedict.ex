cd5b0a704c1ccf7350a9a42fd40e33a538aaa9ae
iwatakeshi <iwatakeshi@gmail.com>
2024-07-02 14:39:41 -0600

    Refactor code organization and separate concerns in core loaders

--- lib/decoders/jmnedict.ex
+++ lib/decoders/jmnedict.ex
@@ -2,38 +2,21 @@ defmodule JMDictEx.Decoders.JMNEDict do
   alias JMDictEx.Models.JMNEDict
   alias JMDictEx.Models.JMNEDict.{Word, TranslationGroup, Translation, Kana, Kanji}
 
-  # Helper functions
-  def decode_word(word) do
-    %{
-      word
-      | kana: Enum.map(word.kana, &struct(Kana, &1)),
-        kanji: Enum.map(word.kanji, &struct(Kanji, &1)),
-        translation: Enum.map(word.translation, &decode_translation_group/1)
-    }
+  def decode(json) do
+    Poison.decode(json,
+      as: %JMNEDict{
+        words: [
+          %Word{
+            kana: [%Kana{}],
+            kanji: [%Kanji{}],
+            translation: [
+              %TranslationGroup{
+                translation: [%Translation{}]
+              }
+            ]
+          }
+        ]
+      }
+    )
   end
-
-  def decode_translation_group(group) do
-    %{group | translation: Enum.map(group.translation, &struct(Translation, &1))}
-  end
-
-  # Poison.Decoder implementations
-  defimpl Poison.Decoder, for: JMNEDict do
-    def decode(dict, _options) do
-      %{dict | words: Enum.map(dict.words, &JMDictEx.Decoders.JMNEDict.decode_word/1)}
-    end
-  end
-
-  defimpl Poison.Decoder, for: Word do
-    def decode(word, _options) do
-      JMDictEx.Decoders.JMNEDict.decode_word(word)
-    end
-  end
-
-  defimpl Poison.Decoder, for: TranslationGroup do
-    def decode(group, _options) do
-      JMDictEx.Decoders.JMNEDict.decode_translation_group(group)
-    end
-  end
-
-  def decode(dict), do: Poison.decode(dict, as: JMNEDict)
 end
---
+++ lib/decoders/radkfile.ex
@@ -0,0 +1,28 @@
+defmodule JMDictEx.Decoders.Radkfile do
+  alias JMDictEx.Models.Radkfile
+  alias JMDictEx.Models.Radkfile.RadicalEntry
+
+   def decode(json) do
+    case Poison.decode(json, as: %Radkfile{
+      radicals: %{}
+    }) do
+      {:ok, decoded} ->
+        result = %{decoded | radicals: transform_radicals(decoded.radicals)}
+        {:ok, result}
+      {:error, _} -> {:error, "Failed to decode Radkfile" }
+    end
+
+  end
+
+  defp transform_radicals(radicals) when is_map(radicals) do
+    Enum.map(radicals, fn {key, value} ->
+      {key, struct(RadicalEntry, %{
+        code: Map.get(value, "code"),
+        kanji: Map.get(value, "kanji", []),
+        stroke_count: Map.get(value, "strokeCount")
+      })}
+    end)
+    |> Enum.into(%{})
+  end
+  defp transform_radicals(_), do: %{}
+end
